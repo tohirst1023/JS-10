dd23902937227145a2613043e1cab833a253063e
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-11 15:04:44 +0500

    rdhfdhds

---
+++ index.js
@@ -0,0 +1,31 @@
+// function twoTimes (num) {
+//     return 2 * num;
+// }
+
+// console.log (twoTimes(3))
+// console.log (twoTimes(5))
+
+
+// function data (soz){
+//     return soz.toUpperCase ();
+// }
+
+
+// console.log (data(Husan))
+// console.log (data(h  usan))
+
+
+
+let son1 = Number(prompt("1 chi soni kiritng"))
+
+let son2 = Number (prompt("2 chi soni kiritng"))
+
+function max(son1,son2) {
+    if (son1 > son2) {
+        return son1
+    }else{
+       return son2
+    }
+}
+
+alert(max(son1,son2))
